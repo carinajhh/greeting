409dbfb6560c00085bd8df94050a3e0decea0932
Carina <carinajh99@gmail.com>
2023-08-04 08:39:10 -0700

    more changes

--- hello.js
+++ hello.js
@@ -1,3 +1,10 @@
-function hello() {
-  return "Hello World!";
+function hello(name) {
+  if (name == "") {
+    return "";
+  }
+  else if (name == name.toUpperCase()) {
+    return "HELLO " + name + "!";
+  }
+  else {
+  return "Hello, " + name; }
 }
--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,15 @@
-var utils  = require('course-utilities');
-var hello = utils.load('./hello.js', 'hello');
+var utils  = require("course-utilities");
+var hello = utils.load('./hello.js', "hello");
 
-test('outputs the correct string', () => {
-  expect(hello()).toBe("Hello World!");
+test("outputs the correct string", () => {
+  expect(hello("Andrew")).toBe("Hello, Andrew");
+  expect(hello("")).toBe("");
+});
+
+test("empty", () => {
+  expect(hello("")).toBe("");
+});
+
+test("shouting", () => {
+  expect(hello("ANDREW")).toBe("HELLO ANDREW!");
 });
